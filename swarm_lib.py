ddf32a063653a941cbb90d0efb7883c7366f70a8
Rishikesh Vanarse <rishikesh.vanarse@gmail.com>
2019-06-24 17:33:20 +0530

    added neigbours and group fn

--- swarm_lib.py
+++ swarm_lib.py
@@ -1,4 +1,5 @@
 """
+Rishikesh Vanarse
 
 Created: 24/06/19
 Last Updated: 24:06/19
@@ -11,22 +12,38 @@ Functions need to be added and optimized
 """
 
 import numpy as np
-
+import math
 
 DEFAULT_NEIGHBOURHOOD_VAL = 5 #Radius of neighbourhood 
+DEFAULT_SIZE = 0.5 #Radius of chassis
+MAX_SPEED = 1.5
+
+
 
 SWARM =[] #List of all bots in the swarm
 
 class Bot:
 
-	def __init__(self, x,y, state= 0, neighbourhood_radius = DEFAULT_NEIGHBOURHOOD_VAL):
-		self.x = x
-		self.y = y
+	def __init__(self, x,y, theta=0, state= 0, neighbourhood_radius = DEFAULT_NEIGHBOURHOOD_VAL):
+		self.x = float(x)
+		self.y = float(y)
+		self.theta = theta
 		self.state = state
 		self.neighbourhood_radius = neighbourhood_radius
-		SWARM.apeend(self)
+		SWARM.append(self)
 
 	def dist(self, bot):
-		return np.sqrt(np.square(self.x - bot.x) + np.square(self.y, bot.y))
+		return math.sqrt((bot.x - self.x)**2 + (bot.y - self.y)**2)
 
+	def neighbours(self):
+		neighbours =[]
+		for b in SWARM and b != self:
+			if b.dist(self) < self.neighbourhood_radius:
+				neighbours.append(b)
+		return neighbours
+
+	def group(self):
+		grp = self.neighbours()
+		grp.append(self)
+		return grp
 
