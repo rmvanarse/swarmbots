bc61cae425b80b4af37862c600e99b85d3a9aad7
Rishikesh Vanarse <rishikesh.vanarse@gmail.com>
2019-06-25 19:49:10 +0530

    Created catkin ws

---
+++ swarmbots_ws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/abc/swarmbots/swarmbots_ws/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/opt/ros/kinetic/share/euslisp/jskeus/eus//Linux64/lib:/opt/ros/kinetic/lib:/opt/ros/kinetic/lib/x86_64-linux-gnu:/opt/ros/kinetic/share/euslisp/jskeus/eus//Linux64/lib"
+export PATH="/opt/ros/kinetic/share/euslisp/jskeus/eus//Linux64/bin:/opt/ros/kinetic/bin:/opt/ros/kinetic/share/euslisp/jskeus/eus//Linux64/bin:/home/abc/bin:/home/abc/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin"
+export PWD="/home/abc/swarmbots/swarmbots_ws/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/abc/swarmbots/swarmbots_ws/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/abc/swarmbots/swarmbots_ws/src:$ROS_PACKAGE_PATH"
