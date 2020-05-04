c32b738c9e9773f556bef42d6664300ff4572ef1
Rishikesh Vanarse <rishikesh.vanarse@gmail.com>
2020-05-04 22:36:29 +0530

    Sim result stat generation added

--- algorithm_simulation/analysis.py
+++ algorithm_simulation/analysis.py
@@ -12,6 +12,7 @@ Performs analysis on simulation results
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import os
 
 #Parameters:
 
@@ -47,17 +48,18 @@ class Result:
 		self.num_simulations = self.df.shape[0]
 		self.info = self.df.columns[4]
 
-		self.score = None
+		self.score = None	#Includes failures
 		self.iterations = None
 		self.success_rate = None
 		
 
-	def split_success_failure(self, df = self.df):
+	def split_success_failure(self):
 		"""
 		Input: df to be split
 		Creates 2 new dataframes for success and failures
 		Returns: Success rate
 		"""
+		df = self.df
 		self.df_fail = df[df['Success']==False]
 		self.df_success = df[df['Success']==True]
 
@@ -65,25 +67,60 @@ class Result:
 
 
 	
-	def success_rate(self, df = self.df):
-		self.success_rate =  df['Success'].mean()
+	def get_success_rate(self):
+		self.success_rate =  self.df['Success'].mean()
 		return self.success_rate
 
 	
 
 	def extra_info_stats(self, verbose=False):
-		info_max = self.df[info].max()
-		info_min = self.df[info].min()
+		info_max = self.df[self.info].max()
+		info_min = self.df[self.info].min()
 
-		info_fail_avg = self.df_fail[info].mean()
+		info_fail_avg = self.df_fail[self.info].mean()
 
 		if(verbose):
-			print("Max "+info+": "+str(info_max))
-			print("Min "+info+": "+str(info_min))
-			print("Mean "+info+" for failures: "+str(info_fail_avg))
+			print("Max "+self.info+": "+str(info_max))
+			print("Min "+self.info+": "+str(info_min))
+			print("Mean "+self.info+" for failures: "+str(info_fail_avg))
+			print("")
 
 		return info_min, info_max, info_fail_avg
 
 	
 	def mean_iter_to_success(self):
-		iterations = self.df_success['Iterations'].mean()
+		self.iterations = self.df_success['Iterations'].mean()
+		return self.iterations
+
+	def max_iter(self):
+		return self.df['Iterations'].max()+1
+
+	def get_score(self):
+		self.score = self.df['Score'].mean()
+		return self.score
+
+	def produce_stats(self):
+		self.split_success_failure()
+		self.get_success_rate()
+		self.mean_iter_to_success()
+		self.max_iter()
+		self.get_score()
+		
+
+	def summary(self):
+		print("No. of simulations:\t", self.num_simulations)
+		print("Max iterations:\t\t", self.max_iter())
+		print("")
+		print("Average score:\t\t", self.score)
+		print("Success rate:\t\t", self.success_rate)
+		print("Avg iter for success:\t", self.iterations)
+		print("")
+		self.extra_info_stats(verbose=True)
+		return None
+
+
+if __name__ == '__main__':
+	result = Result(CSV_PATH+CSV_NAME)
+	result.produce_stats()
+	result.summary()
+
